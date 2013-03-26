a9d1ffffd3f341246ed6bb6d1675e41a24a69402
Isaac Remuant <isaacremuant@gmail.com>
2013-03-26 15:10:22 -0300

    python script to clone or pull specific plugins from repos

---
+++ bundle/clone_plugin_repos.py
@@ -0,0 +1,52 @@
+#!usr/bin/env python
+#coding: utf-8
+import os
+
+repos_dict = {
+    "surround" : "https://github.com/tpope/vim-surround.git",
+    "tcomment" : "https://github.com/tomtom/tcomment_vim.git",
+    "mru" : "https://github.com/vim-scripts/mru.vim.git",
+    "zencoding" : "https://github.com/mattn/zencoding-vim.git",
+     }
+
+# [repos_dict["mru"][repos_dict["mru"].rfind("/")+1:] for x in repos
+# element = repos_dict["mru"]
+# element[element.rfind("/")+1:]
+
+# expected names of directories after git clone <url>
+dirnames = {val[val.rfind("/")+1:-len(".git")]:val for val in repos_dict.values()}
+print(dirnames)
+print()
+
+def main():
+    last = None
+    for name, url in repos_dict.items():
+        # folder_name = 
+       print(name, url)
+       
+    # dirs = os.listdir(".")
+    dirs = dirnames.keys()
+    for x in dirs:
+        # if the directory doesn't exist, create it so you can clone it
+        if not os.path.isdir(x):
+            os.mkdir(x)
+        # enter directory
+        commands = "".join(("cd ",x,";"))
+        print("run ",commands)
+        os.system(commands)
+
+        # if git repo, execute pull 
+        if ".git" in os.listdir(x):
+            commands = "".join(("git pull -v"))
+        else:
+            commands = "".join(("git clone ", dirnames[x]))
+        print("run ",commands)
+        os.system(commands)
+        
+        # exit directory
+        commands = "".join(("cd .."))
+        print("run ",commands)
+        os.system(commands)
+
+if __name__ == "__main__":
+    main()
